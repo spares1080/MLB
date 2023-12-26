86fd7034ddba5d04e1c16c8818b62458f941bf5a
spares1080 <37258481+spares1080@users.noreply.github.com>
2023-12-25 18:18:46 -1000

    Update mlbstreamlit.py

--- mlbstreamlit.py
+++ mlbstreamlit.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/13Rxh8FddZGRlaDff2314Jwh2hM4cj5S-qaBU0aKdVWA/edit#gid=0"
+url = "https://docs.google.com/spreadsheets/d/13Rxh8FddZGRlaDff2314Jwh2hM4cj5S-qaBU0aKdVWA/edit?usp=sharing"
 
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
